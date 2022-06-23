243dd797d3494b73fe2bc21265c99ea0e51f46fd
Philip Park <tilker@users.noreply.github.com>
2022-06-23 21:26:38 +0200

    feat: many small changes. no comment.

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,67 +1,67 @@
 const demoData = [
-    {
-        creationDate: 1653516000000,
-        dueDate: 1654176212000,
-        title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
-        done: false,
-        priority: 3
-    },
-    {
-        creationDate: 1651356000000,
-        dueDate: 1654262612000,
-        title: "Andere Testaufgabe, höchste Priorität",
-        done: false,
-        priority: 1
-    },
-    {
-        creationDate: 1650924000000,
-        dueDate: 1658440800000,
-        title: "Testaufgabe mittlere Priorität",
-        done: false,
-        priority: 2
-    },
-    {
-        creationDate: 1652738400000,
-        dueDate: 1657058400000,
-        title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
-        done: true,
-        priority: 3
-    },
-    {
-        creationDate: 1650924000000,
-        dueDate: 1659132000000,
-        title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
-        done: false,
-        priority: 3
-    },
-    {
-        creationDate: 1648850400000,
-        dueDate: 1646089200000,
-        title: "Andere Testaufgabe, höchste Priorität",
-        done: false,
-        priority: 1
-    },
-    {
-        creationDate: 1651269600000,
-        dueDate: 1663192800000,
-        title: "Testaufgabe mittlere Priorität",
-        done: false,
-        priority: 2
-    },
-    {
-        creationDate: 1651960800000,
-        dueDate: 1660168800000,
-        title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
-        done: true,
-        priority: 3
-    },
-    {
-        creationDate: 1647558000000,
-        dueDate: 1658527200000,
-        title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
-        done: true,
-        priority: 1
-    }
+  {
+    creationDate: 1653516000000,
+    dueDate: 1654176212000,
+    title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
+    done: false,
+    priority: 3,
+  },
+  {
+    creationDate: 1651356000000,
+    dueDate: 1654262612000,
+    title: "Andere Testaufgabe, höchste Priorität",
+    done: false,
+    priority: 1,
+  },
+  {
+    creationDate: 1650924000000,
+    dueDate: 1658440800000,
+    title: "Testaufgabe mittlere Priorität",
+    done: false,
+    priority: 2,
+  },
+  {
+    creationDate: 1652738400000,
+    dueDate: 1657058400000,
+    title: "Dies ist erledigte eine Testaufgabe mit niedrigster Priorität",
+    done: true,
+    priority: 3,
+  },
+  {
+    creationDate: 1650924000000,
+    dueDate: 1659132000000,
+    title: "Dies ist eine Testaufgabe mit niedrigster Priorität",
+    done: false,
+    priority: 3,
+  },
+  {
+    creationDate: 1648850400000,
+    dueDate: 1646089200000,
+    title: "Andere Testaufgabe, höchste Priorität",
+    done: false,
+    priority: 1,
+  },
+  {
+    creationDate: 1651269600000,
+    dueDate: 1663192800000,
+    title: "Testaufgabe mittlere Priorität",
+    done: false,
+    priority: 2,
+  },
+  {
+    creationDate: 1651960800000,
+    dueDate: 1660168800000,
+    title: "Dies ist eine erledigte Testaufgabe mit niedrigster Priorität",
+    done: true,
+    priority: 3,
+  },
+  {
+    creationDate: 1647558000000,
+    dueDate: 1658527200000,
+    title: "Dies ist eine erledigte Testaufgabe mit höchster Priorität",
+    done: true,
+    priority: 1,
+  },
 ];
 
 export default demoData;
