3ba4638b77d40eaea60f6e07f441dec620f25766
Philip Park <tilker@users.noreply.github.com>
2022-06-23 19:04:40 +0200

    feat: many small changes. no comment.

--- public/js/service/todosService.js
+++ public/js/service/todosService.js
@@ -28,6 +28,8 @@ export const deleteTodo = async (todoId) => {
 
 // Render items into template
 const updateList = (todos) => {
+  // TODO: dates in menschlich lesbares format wandeln
+
   const listNode = document.querySelector("#todo-item-template");
   listNode.innerHTML = Mustache.render(
     todos.total ? listTemplate : emptyListTemplate,
@@ -45,3 +47,27 @@ export const loadList = async (sortBy, order, filter) => {
     await updateList([]);
   }
 };
+
+// Mustache.registerHelper("showNiceDate", (dueDate) => {
+//   const locale = navigator.language;
+//   const today = new Date();
+//   const tomorrow = new Date(today);
+//   tomorrow.setDate(tomorrow.getDate() + 1);
+//
+//   const date = new Date(dueDate).toLocaleDateString(locale);
+//   if (date === new Date().toLocaleDateString(locale)) {
+//     return "Heute fällig.";
+//   }
+//   if (date === new Date(tomorrow).toLocaleDateString(locale)) {
+//     return "Morgen fällig.";
+//   }
+//   return `Fällig am ${date}`;
+// });
+//
+// // Set a checkbox to checked if conditions is met
+// Mustache.registerHelper("boxChecker", (done) => {
+//   if (!done) {
+//     return "";
+//   }
+//   return "checked";
+// });
